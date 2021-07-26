eb3c1727c8a8b2bfc7c462b1a01e37a616e2179e
thanh90 <30317015+thanh90@users.noreply.github.com>
2021-07-27 00:38:38 +0900

    picked list actions

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -272,7 +272,7 @@ export default function Home({navigation}) {
         <IdeaRegistrationModal onClose={closeModal}/>
       }
       {
-        !Boolean(openModalToAllowPick) &&
+        Boolean(openModalToAllowPick) &&
         <InfoModal isVisible={Boolean(openModalToAllowPick)} onClose={() => setOpenModalToAllowPick(null)}>
           <Text style={{fontSize: 24, color: '#1D395F', marginBottom: 24}}>
               Pick 초대
@@ -287,16 +287,13 @@ export default function Home({navigation}) {
               {
                 loading ?
                 <CBButton
-                  text='거절'
-                  variant='outlined'
+                  variant='contained'
                   loading={loading}
                   containerStyle={{
                     paddingVertical: 16,
-                    paddingHorizontal: 32,
-                    marginRight: 16,
+                    width: '100%',
                     borderRadius: 50
                   }}
-                  onPress={throttle(rejectPick, 10000, {trailing: false})}
                 />
                 :
                 <>
--- screens/PickedList.tsx
+++ screens/PickedList.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, TouchableOpacity, Linking } from 'react-native';
 import Profile from 'components/Profile';
+import { Icon } from 'react-native-elements';
+
+const PickedUser = ({user}) => (
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <Profile user={user} avatarType='circle'/>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <TouchableOpacity
+                style={{
+                    padding: 8,
+                    marginRight: 8
+                }}
+                onPress={() => Linking.openURL(`sms:${user.phoneNumber}`)}
+            >
+                <Icon
+                    type='material-community'
+                    name='chat-processing-outline'
+                />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={{
+                    padding: 8,
+                }}
+                onPress={() => Linking.openURL(`tel:${user.phoneNumber}`)}
+            >
+                <Icon
+                    type='material-community'
+                    name='phone'
+                />
+            </TouchableOpacity>
+        </View>
+    </View>
+)
 
 const PickedList = ({picks}) => {
 
@@ -11,8 +43,9 @@ const PickedList = ({picks}) => {
 
             <FlatList
                 data={picks}
-                renderItem={({item}) => (<Profile user={item}/>)}
+                renderItem={({item}) => (<PickedUser user={item} />)}
                 keyExtractor={item=>item.uid}
+                ItemSeparatorComponent={() => (<View style={{height: 16}} />)}
             />
 
         </View>
